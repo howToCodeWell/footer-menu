99192fd10afbee639b2dcf35eeb139d13a8779b7
peterfisher <c3T-y7J-vo3-qpg>
2019-12-21 22:48:46 +0000

    Adding giles

---
+++ README.md
@@ -0,0 +1,7 @@
+# How To Code Well footer-menu
+The footer menu of How To Code Well which is shared across multiple sites
+
+# Install
+```bash
+$ npm install --save @howtocodewell/footer-menu
+```
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import menu from '../menu'
+import React, {Component} from 'react';
+import '../src/css/index.css';
+
+class FooterMenu extends Component {
+
+    render() {
+        return (
+            <footer className="flex justify-between border-solid border-t-2 mt-2 pt-4 border-gray-400">
+                <nav id="menuMenu" className="container mx-auto px-4">
+
+                    <div className="flex-none justify-between sm:flex ">
+
+                        <div className="flex-none sm:flex-1 md:flex-auto">
+                            <ul>
+                                {menu.left.map((item, key) =>
+                                    <li><a key={key}
+                                           className="block mt-4 font-bold lg:inline-block lg:mt-0 text-pink-600 hover:underline mr-4"
+                                           href={item.url}>{item.name}</a></li>
+                                )}
+                            </ul>
+                        </div>
+
+                        <div className="flex-none sm:flex-1 md:flex-auto">
+                            <ul>
+                                {menu.middle.map((item, key) =>
+                                    <li><a key={key}
+                                           className="block mt-4 font-bold lg:inline-block lg:mt-0 text-pink-600 hover:underline mr-4"
+                                           href={item.url}>{item.name}</a></li>
+                                )}
+                            </ul>
+                        </div>
+
+                        <div className="flex-none sm:flex-1 md:flex-auto">
+                            <ul>
+                                {menu.right.map((item, key) =>
+                                    <li><a key={key}
+                                           className="block mt-4 font-bold lg:inline-block lg:mt-0 text-pink-600 hover:underline mr-4"
+                                           href={item.url}>{item.name}</a></li>
+                                )}
+                            </ul>
+                        </div>
+
+                    </div>
+                </nav>
+            </footer>
+        )
+    }
+}
+
+export default FooterMenu;
